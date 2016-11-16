c149bac6e355b5ae1d39e26d6fb5b6fbaec876ce
Ben Webb <ben@salilab.org>
2016-11-15 18:12:04 -0800

    Add basic tests of the complete algorithm.

--- test/test_basic.py
+++ test/test_basic.py
@@ -4,6 +4,7 @@ import sys
 import utils
 import contextlib
 import subprocess
+import shutil
 
 TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(TOPDIR)
@@ -29,6 +30,34 @@ class Tests(unittest.TestCase):
         """Test simple import"""
         with mock_mdp_package():
             import MiST
+
+    def test_complete_no_training(self):
+        """Test complete run, no training"""
+        with utils.temporary_working_directory() as tmpdir:
+            shutil.copy(os.path.join(TOPDIR, 'test', 'input', 'input.tsv'), '.')
+            subprocess.check_call(['MiST.py', 'input.tsv', 'test', '0', '0'])
+            with open('test.log') as fh:
+                contents = fh.readlines()
+            self.assertTrue('Number of Preys: 5\n' in contents)
+            self.assertTrue('Number of Experiments: 6\n' in contents)
+            self.assertTrue('Number of Baits: 3\n' in contents)
+            os.unlink("test.log")
+            os.unlink("test_metrics.out")
+            os.unlink("test_mist.out")
+
+    def test_complete_training(self):
+        """Test complete run, with training"""
+        with utils.temporary_working_directory() as tmpdir:
+            shutil.copy(os.path.join(TOPDIR, 'test', 'input', 'input.tsv'), '.')
+            subprocess.check_call(['MiST.py', 'input.tsv', 'test', '0', '1'])
+            with open('test.log') as fh:
+                contents = fh.readlines()
+            self.assertTrue('Number of Preys: 5\n' in contents)
+            self.assertTrue('Number of Experiments: 6\n' in contents)
+            self.assertTrue('Number of Baits: 3\n' in contents)
+            os.unlink("test.log")
+            os.unlink("test_metrics.out")
+            os.unlink("test_mist.out")
 
 if __name__ == '__main__':
     unittest.main()
